3e2efff870b9dba23e4e73fcdf6b2ddef9558b89
Ic4rus <thanght4391@gmail.com>
2019-04-20 16:53:57 +0700

    Uploading files

--- src/main/java/com/icarus/guide/GuideApplication.java
+++ src/main/java/com/icarus/guide/GuideApplication.java
@@ -1,13 +1,28 @@
 package com.icarus.guide;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.icarus.guide.hello.storage.StorageProperties;
+import com.icarus.guide.hello.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class GuideApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuideApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 
 }
