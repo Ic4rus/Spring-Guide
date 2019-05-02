56892fe6d390f9e5116a0dba366646d526617083
Ic4rus <thanght4391@gmail.com>
2019-05-02 08:42:31 +0700

    Messaging with JMS

---
+++ src/main/java/com/icarus/guide/hello/Email.java
@@ -0,0 +1,40 @@
+package com.icarus.guide.hello;
+
+public class Email {
+	
+	private String to;
+	private String body;
+	
+	public Email() {
+	}
+	
+	public Email(String to, String body) {
+		this.to = to;
+		this.body = body;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	@Override
+	public String toString() {
+		return "Email{" +
+				"to='" + getTo() + "'" +
+				", body='" + getBody() + "'" +
+				"}";
+	}
+
+}
