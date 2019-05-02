dca6290e93a205e4966046d9cea77005b0664648
Ic4rus <thanght4391@gmail.com>
2019-05-02 10:47:51 +0700

    Creating a Batch Service

---
+++ src/main/java/com/icarus/guide/hello/Person.java
@@ -0,0 +1,41 @@
+package com.icarus.guide.hello;
+
+public class Person {
+	
+	private String firstName;
+	
+	private String lastName;
+
+	public Person() {
+	}
+
+	public Person(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"firstName='" + getFirstName() + "'" +
+				", lastName='" + getLastName() + "'" +
+				"}";
+	}
+
+}
