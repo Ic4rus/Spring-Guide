ea18e32ff6a2dc030faa9ec64374da8c3c996885
Ic4rus <thanght4391@gmail.com>
2019-05-04 12:28:20 +0700

    Integrating

--- src/main/java/com/icarus/guide/GuideApplication.java
+++ src/main/java/com/icarus/guide/GuideApplication.java
@@ -1,13 +1,21 @@
 package com.icarus.guide;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("/com/icarus/guide/hello/integration.xml")
 public class GuideApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GuideApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		ConfigurableApplicationContext ctx = new SpringApplication(GuideApplication.class).run(args);
+		System.out.println("Hit Enter to terminate");
+		System.in.read();
+		ctx.close();
 	}
 
 }
